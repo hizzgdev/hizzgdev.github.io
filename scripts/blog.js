1d5d943b72ddee56ea1e2dd0cfec2d76dea396d3
hizzgdev <hizzgdev@163.com>
2014-07-02 23:45:22 +0800

    add comments

--- scripts/blog.js
+++ scripts/blog.js
@@ -5,6 +5,7 @@ var path_posts = "blog/posts"
 // path_prefix = "https://api.github.com/repos/sneezry/sneezry.github.com/contents/"
 // path_posts = "md"
 
+if (typeof(config) == 'undefined') { config = {lang:'zh'}; }
 var page_size = 3;
 
 var $html = document.getElementsByTagName('html')[0];
@@ -14,6 +15,7 @@ var $template = document.getElementById('blog_item_template');
 
 var __all = null;
 var __all_len = 0;
+
 var __posts = {};
 var __page = 0;
 var __page_total = 1;
@@ -21,8 +23,25 @@ var __page_total = 1;
 var _first_load = true;
 
 function cache_list(resp){
-    __all = resp.data.reverse();
-    __all_len = __all.length;
+    var all_zh = resp.data.reverse();
+    var all_len_zh = all_zh.length;
+    var all_en = [];
+    var all_len_en = 0;
+
+    var i = all_len_zh;
+    var post = null;
+    while(i--){
+        post = all_zh[i];
+        if(/.en.md$/.test(post.name)){
+            all_zh.splice(i,1);
+            all_len_zh--;
+            all_en.unshift(post)
+            all_len_en++;
+        }
+    }
+    __all = config.lang == 'zh' ? all_zh : all_en;
+    __all_len = config.lang == 'zh' ? all_len_zh : all_len_en;
+
     __page_total = Math.ceil(__all_len/page_size);
     show_list(1);
 }
@@ -72,10 +91,10 @@ function show_post_meta(post){
     el.id = 'blog_'+post.sha;
     $container.appendChild(el);
     var post_date = post.name.substr(0,10);
-    var post_name = post.name.substr(11);
-    $('h3 a',el).text(post_name).attr('href','#!/'+post.path);
+    var post_name = post.name.substr(11).replace(/-/g,' ').replace(/\.en\.md/,'').replace(/\.md/,'');
+    $('h3 a',el).text(post_name).attr('href','#!'+post.path);
     $('span.date',el).text(post_date);
-    $('span.ds-thread-count',el).attr('data-thread-key',post.sha);
+    $('a.ds-thread-count',el).attr('data-thread-key',post.sha).click(function(e){toggle_comments(e);return false;});
 }
 
 function show_post(resp){
@@ -95,7 +114,23 @@ function query_post(path){
         var el = document.createElement('script');
         el.src = path_prefix+path+path_suffix;
         $html.appendChild(el);
-        $html.removeChild(el);
+        setTimeout(function(){$html.removeChild(el);},10000);
+    }
+}
+
+function toggle_comments(e){
+    var el = $(e.target);
+    var k = el.attr('data-thread-key');
+    if(el.attr('comment-shown') == 'true'){
+        $('#blog_'+k+' .comments').remove();
+        el.attr('comment-shown', 'false')
+    }else{
+        var c = document.createElement('div');
+        c.className = 'panel-footer comments';
+        c.setAttribute('data-thread-key', k);
+        DUOSHUO.EmbedThread(c);
+        $(c).appendTo('#blog_'+k+'');
+        el.attr('comment-shown', 'true')
     }
 }
 
@@ -103,7 +138,7 @@ function query_post(path){
     var el = document.createElement('script');
     el.src = path_prefix+path+"?callback=cache_list";
     $html.appendChild(el);
-    $html.removeChild(el);
+    setTimeout(function(){$html.removeChild(el);},10000);
 })(path_posts);
 
 (function page_load(){
