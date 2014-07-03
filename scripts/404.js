a61869a5c956a89bf0b89d32a5cdfe2225bfdca5
hizzgdev <hizzgdev@163.com>
2014-07-03 21:55:03 +0800

    debug 404.js

--- scripts/404.js
+++ scripts/404.js
@@ -1,1 +1,8 @@
-console.log(location);
+(function(){
+    var path = location.pathname;
+    if(! /^\/path/.test(path)){
+        console.log('redirect');
+    }else{
+        console.log('ok');
+    }
+})();
